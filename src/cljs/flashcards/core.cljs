c2b02288f6574b58b84bcec7a60d696024446526
Alex King <alex@alexanderjamesking.com>
2018-03-04 11:17:08 +0000

    tidied up

--- src/cljs/flashcards/core.cljs
+++ src/cljs/flashcards/core.cljs
@@ -1,100 +1,11 @@
 (ns flashcards.core
   (:require [reagent.core :as reagent]
-            [re-frame.core :as rf]
-            [flashcards.game :as game]
             [flashcards.routes :as routes]
-            [flashcards.content :as content]
-            [flashcards.view.home :as home]
-            [flashcards.view.play :as play]
-            [flashcards.view.end :as end]
-            [bidi.bidi :as bidi]
-            [pushy.core :as pushy]
-            [clojure.set :refer [map-invert]])  )
+            [flashcards.game-logic :as game-logic]))
 
 (enable-console-print!)
-
-(def cards (merge content/verbs-p1 content/verbs-p2))
-
-(defn- initial-game-state []
-  {:route :home
-   :questions (let [n-questions 5
-                    n-possible-answers 5]
-                (game/generate-shuffled-questions cards
-                                                  n-questions
-                                                  n-possible-answers))
-   :current-question nil
-   :score {:correct 0
-           :incorrect 0}})
-
-(rf/reg-event-fx
- :initialize
- (fn [{:keys [db]} _]
-   {:db (initial-game-state)
-    :dispatch [:move-to-next-question]}))
-
-(rf/reg-event-fx
- :reset-game-state
- (fn [{:keys [db]}]
-   {:db (initial-game-state)
-    :dispatch-n [[:move-to-next-question]
-                 [:navigate-to :play]]}))
-
-(rf/reg-event-fx
- :move-to-next-question
- (fn [{:keys [db]} _]
-   (let [questions (:questions db)
-         current-question (first questions)]
-     (if current-question
-       {:db (merge db
-                   {:questions (rest questions)
-                    :current-question (first questions)})}
-       {:dispatch [:navigate-to :end]}))))
-
-(rf/reg-sub
- :current-question
- (fn [db _]
-   (:current-question db)))
-
-(rf/reg-sub
- :score
- (fn [db _]
-   (:score db)))
-
-(rf/reg-sub
- :next-question
- (fn [db _]
-   (-> (:questions db)
-       shuffle
-       first)))
-
-(rf/reg-sub
- :lookup-wrong-answer
- (fn [db [_ users-answer]]
-   (->> cards
-        (filter (fn [[k v]]
-                  (= users-answer v)))
-        first)))
-
-(rf/reg-event-fx
- :auto-move-to-next-question
- (fn [_ [_ question]]
-   (let [current-question @(rf/subscribe [:current-question])]
-     (if (= (:question question) (:question current-question))
-       {:dispatch [:move-to-next-question]}
-       {}))))
-
-(rf/reg-event-fx
- :answer-question
- (fn [{:keys [db]} [_ guess]]
-   (let [question @(rf/subscribe [:current-question])
-         correct? (= (:correct-answer question) guess)]
-     {:db (-> db
-              (assoc-in [:current-question :answered] {:guess guess :correct? correct?})
-              (update-in [:score (if correct? :correct :incorrect)] inc))
-      :dispatch-later [{:ms (if correct? 2200 5000)
-                        :dispatch [:auto-move-to-next-question question]}]})))
 
 (reagent/render-component [routes/render-app]
                           (. js/document (getElementById "app")))
 
-(rf/dispatch-sync [:initialize])
+(game-logic/init)
